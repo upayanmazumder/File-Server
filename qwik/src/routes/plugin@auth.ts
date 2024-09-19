2d15673fa34b346008c589963fe5328c229e9c4d
upayanmazumder <upayanm3@gmail.com>
2024-09-19 23:39:39 +0530

    feat(auth): add Google provider to QwikAuth
    - Added the Google provider to the QwikAuth module in order to support authentication with Google accounts.

--- qwik/src/routes/plugin@auth.ts
+++ qwik/src/routes/plugin@auth.ts
@@ -1,8 +1,9 @@
 import { QwikAuth$ } from "@auth/qwik";
 import GitHub from "@auth/qwik/providers/github";
+import Google from "@auth/qwik/providers/google";
 
 export const { onRequest, useSession, useSignIn, useSignOut } = QwikAuth$(
   () => ({
-    providers: [GitHub],
+    providers: [GitHub, Google],
   }),
 );
