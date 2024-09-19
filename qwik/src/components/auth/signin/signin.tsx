d76b6f79ecc417fbe93f72bdbfb92c4727dbc35d
upayanmazumder <upayanm3@gmail.com>
2024-09-20 00:09:01 +0530

    Refactor: Update signin component to add support for Google provider

--- qwik/src/components/auth/signin/signin.tsx
+++ qwik/src/components/auth/signin/signin.tsx
@@ -5,10 +5,20 @@ import { useSignIn } from '~/routes/plugin@auth';
 export default component$(() => {
   const signIn = useSignIn();
   return (
-    <Form action={signIn}>
-      <input type="hidden" name="providerId" value="github" />
-      <input type="hidden" name="options.redirectTo" value="https://fs.upayan.space/" />
-      <button>Sign In</button>
-    </Form>
+    <>
+
+        <Form action={signIn}>
+        <input type="hidden" name="providerId" value="github" />
+        <input type="hidden" name="options.redirectTo" value="https://fs.upayan.space/" />
+        <button>Sign-in with Github</button>
+        </Form>
+
+        <Form action={signIn}>
+        <input type="hidden" name="providerId" value="google" />
+        <input type="hidden" name="options.redirectTo" value="https://fs.upayan.space/" />
+        <button>Sign-in with Google</button>
+        </Form>
+
+    </>
   );
 });
