c4552925ea1f28509c3b0daf9c9c8ed1318d7dc9
upayanmazumder <upayanm3@gmail.com>
2024-09-20 13:41:08 +0530

    removed auth component and replaced it with route

--- qwik/src/routes/index.tsx
+++ qwik/src/routes/index.tsx
@@ -2,9 +2,12 @@ import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 import Infobox from "../components/infobox/infobox";
-import Signin from "../components/auth/signin/signin";
+
+import { Form } from '@builder.io/qwik-city';
+import { useSignIn } from '~/routes/plugin@auth';
 
 export default component$(() => {
+  const signIn = useSignIn();
   return (
     <>
    
@@ -13,7 +16,23 @@ export default component$(() => {
       <div role="presentation" class="ellipsis ellipsis-purple"></div>
 
       <div class="container container-center container-spacing-xl">
-      <Signin />
+      <Form action={signIn}>
+        <input type="hidden" name="providerId" value="github" />
+        <input type="hidden" name="options.redirectTo" value="https://fs.upayan.space/" />
+        <button>Sign-in with Github</button>
+        </Form>
+
+        <Form action={signIn}>
+        <input type="hidden" name="providerId" value="google" />
+        <input type="hidden" name="options.redirectTo" value="https://fs.upayan.space/" />
+        <button>Sign-in with Google</button>
+        </Form>
+
+        <Form action={signIn}>
+        <input type="hidden" name="providerId" value="discord" />
+        <input type="hidden" name="options.redirectTo" value="https://fs.upayan.space/" />
+        <button>Sign-in with Discord</button>
+        </Form>
         <h3>
           You can <span class="highlight">count</span>
           <br /> on me
