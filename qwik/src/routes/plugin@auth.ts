64575670662c0109e61a446b1ad187da4690fbae
upayanmazumder <upayanm3@gmail.com>
2024-09-20 13:12:06 +0530

    fix

--- qwik/src/routes/plugin@auth.ts
+++ qwik/src/routes/plugin@auth.ts
@@ -5,6 +5,19 @@ import Discord from "@auth/qwik/providers/discord";
 
 export const { onRequest, useSession, useSignIn, useSignOut } = QwikAuth$(
   () => ({
-    providers: [GitHub, Google, Discord],
+    providers: [
+      GitHub({
+        clientId: process.env.GITHUB_ID,
+        clientSecret: process.env.GITHUB_SECRET,
+      }),
+      Google({
+        clientId: process.env.GOOGLE_ID,
+        clientSecret: process.env.GOOGLE_SECRET,
+      }),
+      Discord({
+        clientId: process.env.DISCORD_ID,
+        clientSecret: process.env.DISCORD_SECRET,
+      }),
+    ],
   }),
 );
