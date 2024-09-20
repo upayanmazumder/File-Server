7683d5e4a6e87a3cdda39212f692ea7191d063dc
upayanmazumder <upayanm3@gmail.com>
2024-09-20 23:12:48 +0530

    Refactor session component to include sign-in and sign-out functionality

--- qwik/src/components/auth/session/session.tsx
+++ qwik/src/components/auth/session/session.tsx
@@ -1,17 +1,36 @@
 import { component$ } from '@builder.io/qwik';
 import { useSession } from '~/routes/plugin@auth';
 import sessionStyles from "./session.module.css";
+import SignOut from '../sign-out';
+import SignIn from '../sign-in/sign-in';
+
 export default component$(() => {
   const session = useSession();
-  const imageUrl = session.value?.user?.image ?? '';
+
+  // Check if the user is signed in
+  const isSignedIn = session.value?.user;
+
   return (
     <div class={sessionStyles.wrapper}>
-      <img width="80" height="80" src={imageUrl} loading="lazy"  alt={session.value?.user?.name ?? 'User Icon'} class={sessionStyles.profilepic}></img>
-      <div class={sessionStyles.info}>
-        <p class={sessionStyles.name}>{session.value?.user?.name}</p>
-        <p class={sessionStyles.email}>{session.value?.user?.email}</p>
-      </div>
+      {isSignedIn ? (
+        <>
+          <img 
+            width="80" 
+            height="80" 
+            src={session.value.user?.image ?? ''} 
+            loading="lazy"  
+            alt={session.value.user?.name ?? 'User Icon'} 
+            class={sessionStyles.profilepic} 
+          />
+          <div class={sessionStyles.info}>
+            <p class={sessionStyles.name}>{session.value.user?.name}</p>
+            <p class={sessionStyles.email}>{session.value.user?.email}</p>
+          </div>
+          <SignOut />
+        </>
+      ) : (
+        <SignIn />
+      )}
     </div>
   );
 });
- 
