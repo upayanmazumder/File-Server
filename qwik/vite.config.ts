513755b4dfe16d75a79a3d1d9d081bf26d63688e
upayanmazumder <upayanm3@gmail.com>
2024-09-24 10:45:04 +0530

    Refactor: Exclude 'firebase-admin' from external dependencies in ssr configuration

--- qwik/vite.config.ts
+++ qwik/vite.config.ts
@@ -22,7 +22,7 @@ errorOnDuplicatesPkgDeps(devDependencies, dependencies);
 export default defineConfig((): UserConfig => {
   return {
     ssr: {
-      noExternal: [],
+      noExternal: ['firebase-admin'],
     },
     plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
     // This tells Vite which dependencies to pre-build in dev mode.
