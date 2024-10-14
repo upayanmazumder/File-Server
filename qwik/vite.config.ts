6ed63beba7c8a9e524289034e215262246cd7236
upayanmazumder <upayanm3@gmail.com>
2024-10-15 02:32:07 +0530

    added devdependencies

--- qwik/vite.config.ts
+++ qwik/vite.config.ts
@@ -32,24 +32,31 @@ export default defineConfig((): UserConfig => {
     /**
      * This is an advanced setting. It improves the bundling of your server code. To use it, make sure you understand when your consumed packages are dependencies or dev dependencies. (otherwise things will break in production)
      */
-    // ssr:
-    //   command === "build" && mode === "production"
-    //     ? {
-    //         // All dev dependencies should be bundled in the server build
-    //         noExternal: Object.keys(devDependencies),
-    //         // Anything marked as a dependency will not be bundled
-    //         // These should only be production binary deps (including deps of deps), CLI deps, and their module graph
-    //         // If a dep-of-dep needs to be external, add it here
-    //         // For example, if something uses `bcrypt` but you don't have it as a dep, you can write
-    //         // external: [...Object.keys(dependencies), 'bcrypt']
-    //         external: Object.keys(dependencies),
-    //       }
-    //     : undefined,
     ssr: {
       noExternal: Object.keys(devDependencies),
-      external: [...Object.keys(dependencies), 'fs'], // Add 'fs' here
+      external: [
+        ...Object.keys(dependencies),
+        'fs',
+        'buffer',
+        'stream',
+        'events',
+        'assert',
+        'tls',
+        'net',
+        'util',
+        'path',
+        'querystring',
+        'crypto',
+        'child_process',
+        'os',
+        'url',
+        'https',
+        'zlib',
+        'http2',
+        'http'
+      ],
     },
-    
+
     server: {
       headers: {
         // Don't cache the server response in dev mode
