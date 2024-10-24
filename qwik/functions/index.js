d70b47098ef227e182fad890b2b955c830b04474
upayanmazumder <upayanm3@gmail.com>
2024-10-24 21:12:37 +0530

    ggg

--- qwik/functions/index.js
+++ qwik/functions/index.js
@@ -1,5 +1,5 @@
 import { https } from "firebase-functions";
-import qwikApp from './server/entry-firebase.mjs';
+import qwikApp from './server/entry-firebase.js';
 
 
 export const app = https.onRequest(qwikApp)
