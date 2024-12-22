44a1b683c59741c574ee5e36a6bc12f0c2181c13
Upayan Mazumder <upayanm3@gmail.com>
2024-12-23 01:04:40 +0530

    Remove obsolete authentication pages and update header branding

--- app/src/components/header/header.js
+++ app/src/components/header/header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaGithub, FaLink, FaDiscord, FaCloud, FaSignInAlt } from 'react-icons/fa';
+import { FaGithub, FaLink, FaDiscord, FaSignInAlt } from 'react-icons/fa';
 import styles from './header.module.css';
 import Link from 'next/link';
 
@@ -8,36 +8,18 @@ const Header = () => {
     <header className={styles.header}>
       <div className={styles.container}>
         <Link href="/" className={styles.logoContainer}>
-          <img src="/cas.svg" alt="CAS Logo" className={styles.icon} />
-          <h1 className={styles.title}>CAS</h1>
+          <img src="/file.svg" alt="File Server Logo" className={styles.icon} />
+          <h1 className={styles.title}>File Server</h1>
         </Link>
         <div className={styles.buttonContainer}>
           <a
-            href="https://github.com/upayanmazumder/Collaborative-Article-Sharing"
+            href="https://github.com/upayanmazumder/File-Server"
             target="_blank"
             rel="noopener noreferrer"
             className={styles.button}
           >
             <FaGithub className={styles.iconStyle} />
             GitHub Repo
-          </a>
-          <a
-            href="https://api.cas.upayan.dev"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.button}
-          >
-            <FaCloud className={styles.iconStyle} />
-            API Link
-          </a>
-          <a
-            href="https://pypi.org/project/collaborative-article-sharing/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.button}
-          >
-            <FaLink className={styles.iconStyle} />
-            Pypi Repository
           </a>
           <a
             href="https://discord.gg/wQTZcXpcaY"
